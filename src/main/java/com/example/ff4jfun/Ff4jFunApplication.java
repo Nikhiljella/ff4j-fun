2d02dc76a0d894203abc41dbc1b0a416334e57b0
jella nikhil babu <nikhilpateljella@gmail.com>
2022-07-09 23:09:45 +0530

    "ff4j fun starts "

---
+++ src/main/java/com/example/ff4jfun/Ff4jFunApplication.java
@@ -0,0 +1,13 @@
+package com.example.ff4jfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ff4jFunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ff4jFunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ff4jfun/Ff4jFunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ff4jfun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ff4jFunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
